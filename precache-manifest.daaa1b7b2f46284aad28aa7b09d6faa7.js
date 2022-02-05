69802bf23448d9d4da3bf2b16d00570c82cbdeaa
Sanusi Abdulkadir <sanusiabdulkadir00@gmail.com>
2022-02-05 23:20:53 +0100

    Updates

---
+++ precache-manifest.daaa1b7b2f46284aad28aa7b09d6faa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffeae8592cd5b4c6d7d9c574b74d15f",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "8a080293182d8d423669",
+    "url": "/e-commerce/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "912a5b386f2395d4982c",
+    "url": "/e-commerce/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "8a080293182d8d423669",
+    "url": "/e-commerce/static/js/2.2fbcbdde.chunk.js"
+  },
+  {
+    "revision": "912a5b386f2395d4982c",
+    "url": "/e-commerce/static/js/main.ff89f2c0.chunk.js"
+  },
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/e-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
